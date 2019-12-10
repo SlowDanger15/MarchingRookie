61bd1b9ee82ec0193a33504b3575ce87ede87804
SlowDanger15 <58740045+SlowDanger15@users.noreply.github.com>
2019-12-10 22:21:54 +0530

    Add files via upload
    this is a program

--- happy.py
+++ happy.py
@@ -1,1 +1,23 @@
-n=int(input('Enter a number th check'))
+num=int(input('Enter a number to check whether a number is happy or not'))
+lst = list()
+lst.append(num)
+flag = 0
+n= num
+while True:
+    sum=0
+    while n>0:
+        r=n%10
+        sum=sum+r*r
+        n=int(n/10)
+    if sum in lst:
+        break
+    if sum==1:
+        flag = 1
+        break
+    else:
+        n = sum
+    lst.append(sum)
+if flag==1:
+    print('HAPPY NUMBER')
+else:
+    print('UNHAPPY NUMBER')
